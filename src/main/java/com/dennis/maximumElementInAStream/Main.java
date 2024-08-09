41768ce91a4190d94041d0bcb8885f682dbd5d45
denizmustafabektas <denizmustafabektas@gmail.com>
2024-08-09 17:11:10 +0300

    DSA Heap Leetcode Questions

---
+++ src/main/java/com/dennis/maximumElementInAStream/Main.java
@@ -0,0 +1,83 @@
+package com.dennis.maximumElementInAStream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static List<Integer> streamMax(int[] nums) {
+
+        if (nums.length == 0) {
+            return null;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        Heap heap = new Heap();
+
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if(i == 0) {
+                list.add(nums[0]);
+            } else {
+                if(max <= nums[i]) {
+                    max = nums[i];
+                    list.add(max);
+                } else {
+                    list.add(max);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        // Test case 1
+        int[] nums1 = {1, 5, 2, 9, 3, 6, 8};
+        System.out.println("Test case 1:");
+        System.out.println("Input: [1, 5, 2, 9, 3, 6, 8]");
+        System.out.println("Expected output: [1, 5, 5, 9, 9, 9, 9]");
+        System.out.println("Actual output: " + streamMax(nums1));
+        System.out.println();
+
+        // Test case 2
+        int[] nums2 = {10, 2, 5, 1, 0, 11, 6};
+        System.out.println("Test case 2:");
+        System.out.println("Input: [10, 2, 5, 1, 0, 11, 6]");
+        System.out.println("Expected output: [10, 10, 10, 10, 10, 11, 11]");
+        System.out.println("Actual output: " + streamMax(nums2));
+        System.out.println();
+
+        // Test case 3
+        int[] nums3 = {3, 3, 3, 3, 3};
+        System.out.println("Test case 3:");
+        System.out.println("Input: [3, 3, 3, 3, 3]");
+        System.out.println("Expected output: [3, 3, 3, 3, 3]");
+        System.out.println("Actual output: " + streamMax(nums3));
+        System.out.println();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Test case 1:
+            Input: [1, 5, 2, 9, 3, 6, 8]
+            Expected output: [1, 5, 5, 9, 9, 9, 9]
+            Actual output: [1, 5, 5, 9, 9, 9, 9]
+
+            Test case 2:
+            Input: [10, 2, 5, 1, 0, 11, 6]
+            Expected output: [10, 10, 10, 10, 10, 11, 11]
+            Actual output: [10, 10, 10, 10, 10, 11, 11]
+
+            Test case 3:
+            Input: [3, 3, 3, 3, 3]
+            Expected output: [3, 3, 3, 3, 3]
+            Actual output: [3, 3, 3, 3, 3]
+
+        */
+
+    }
+
+}
+
